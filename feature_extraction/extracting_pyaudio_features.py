292cf396b8f5c18ee57214b46dca07dd6cf92eea
adibhouraskar2606 <adi.bhouraskar@gmail.com>
2020-11-17 02:20:23 -0700

    feature extraction

--- feature_extraction/extracting_pyaudio_features.py
+++ feature_extraction/extracting_pyaudio_features.py
@@ -1,18 +1,19 @@
 #Run with python version 2
 import os
 import glob
+import pydub
+import matplotlib
 import numpy as np
 
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
 
-
 def create_mother_vector(file_name):
     #This function extracts 34 features for each .wav file.
 
-    [Fs, x] = audioBasicIO.readAudioFile(file_name);
-    F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.020*Fs, 0.020*Fs);
-    features = np.mean(F.T, axis=0) 
+    [Fs, x] = audioBasicIO.readAudioFile(file_name)
+    F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.020*Fs, 0.020*Fs)
+    features = np.mean(F[0].T, axis=0)
      
     #mother vector is the vector which contains all 34 features.
     mother_vector = np.hstack([features])
@@ -24,28 +25,24 @@ def create_mother_vector(file_name):
     new_path_file = os.path.join(add_path, new_name)
     np.save(new_path_file, mother_vector)
 
-
 def generating_mother_vector_files_bee():
     #change the names of directories for train, test and validate accordingly.
-    os.chdir("/home/chelsi/BUZZ1/train/bee_train/")
+    os.chdir("/home/adi/Adi_research_data/BUZZ3/train/bee_train")
     for filename in glob.glob('*.wav'):
         create_mother_vector(filename)
-
 
 def generating_mother_vector_files_noise():
     #change the names of directories for train, test and validate accordingly.
-    os.chdir("/home/chelsi/BUZZ1/train/noise_train/")
+    os.chdir("/home/adi/Adi_research_data/BUZZ3/train/noise_train")
     for filename in glob.glob('*.wav'):
         create_mother_vector(filename)
-
 
 def generating_mother_vector_files_cricket():
     #change the names of directories for train, test and validate accordingly.
-    os.chdir("/home/chelsi/BUZZ1/train/cricket_train/")
+    os.chdir("/home/adi/Adi_research_data/BUZZ3/train/cricket_train")
     for filename in glob.glob('*.wav'):
         create_mother_vector(filename)
 
-
-#generating_mother_vector_files_bee()
-#generating_mother_vector_files_noise()
-#generating_mother_vector_files_cricket()
+generating_mother_vector_files_bee()
+generating_mother_vector_files_noise()
+generating_mother_vector_files_cricket()
